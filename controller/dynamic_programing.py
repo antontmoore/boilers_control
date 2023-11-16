680a18debf9b4ae13cfd5a7b86db34d3ada76045
Anton Tmur <tmur.a@numdes.com>
2023-11-16 22:58:37 +0000

    dynamic programming controller added

--- controller/dynamic_programing.py
+++ controller/dynamic_programing.py
@@ -17,10 +17,10 @@ from math import ceil
 
 class DynamicProgramingController(Controller):
     """
-        Dynamic Programing controller.
-
-        Splits work periods into pieces for each house.
-        It boils only before consumption starts.
+        Dynamic Programing controller. Controller has the horizon and split it into 15min intervals.
+        Then it calculate the dp-table, which has dimensions [number_of_temperature_steps, number_of_15min_timesteps].
+        After dynamic programming calculation of dp-table controller restores the optimal path and
+        make the schedule for every single house.
     """
 
     def __init__(self,
@@ -40,7 +40,8 @@ class DynamicProgramingController(Controller):
                          boiler_capacity__m3,
                          max_instantaneous_power__W,
                          max_15min_power__W)
-        self.horizon__steps = int(self.horizon_time__sec / DEFAULT_TIME_STEP__sec)
+        self.horizon_steps = int(self.horizon_time__sec / DEFAULT_TIME_STEP__sec)
+        self.horizon_periods = int(self.horizon_time__sec / 15 / 60)
 
     def generate_control(self,
                          start_temperatures__degC: NDArray,
@@ -57,210 +58,175 @@ class DynamicProgramingController(Controller):
             :return: control                      control actions for all houses       [horizon_steps, number_of_houses]
         """
 
-        self.horizon__steps = int(self.horizon_time__sec / step_size__sec)
+        period_15min__sec = 15 * 60
+        self.horizon_steps = int(self.horizon_time__sec / step_size__sec)
+        self.horizon_periods = int(self.horizon_time__sec / period_15min__sec)
+        steps_inside_period = int(period_15min__sec / step_size__sec)
 
-        one_heater_power__W = self.model[0].heater_power__W
-        allowable_energy_adding__J = [p * one_heater_power__W * step_size__sec for p in range(int(self.max_instantaneous_power__W / one_heater_power__W))]
-        max_allowable_energy_15min__J = 15 * 60 * self.max_15min_power__W
+        one_step_heater_energy__J = self.heater_power__W * self.model[0].efficiency * step_size__sec
 
+        def get_allowable_control():
+            """
+                Function calculating max control steps inside 15 min period.
+                Needed to calculate how many step_sizes and how many boilers we can switch on inside 15 min period.
+            """
+
+            max_heaters_in_step = int(self.max_instantaneous_power__W / self.heater_power__W)
+
+            max_control_steps = min(
+                int(self.max_15min_power__W * steps_inside_period / self.heater_power__W),
+                max_heaters_in_step * steps_inside_period
+            )
+            return max_control_steps
+
+        def interpolate_to_horizon(x, y):
+            """
+                Function to interpolate water consumption and energy price for control horizon.
+
+                :param x: array of timestamps, corresponding to values
+                :param y: array of values
+                :return: array of values corresponfing to every 15 min period inside horizon
+            """
+
+            x = np.hstack((x, x + 24*3600))
+            y = np.hstack((y, y))
+            timestamps__sec = np.linspace(0, self.horizon_time__sec, self.horizon_time__sec + 1) + current_time__sec
+            detailed_values = np.interp(timestamps__sec, x, y)
+            result = np.zeros((int(self.horizon_time__sec / period_15min__sec),))
+            for j in range(result.shape[0]):
+                result[j] = np.sum(detailed_values[j * period_15min__sec: (j+1)*period_15min__sec])
+
+            return result
+
+        # allowable control actions are 0 and max power due to constraints
+        max_control_steps_inside_15min = get_allowable_control()
+        allowable_heating_steps = [0, max_control_steps_inside_15min]
+
+        # interpolate external data of energy price and water consumption
+        horizon_price__usd_per_J = interpolate_to_horizon(energy_price__usd_per_J[:, 0],
+                                                          energy_price__usd_per_J[:, 1])
+
+        horizon_consumption__m3 = interpolate_to_horizon(water_consumption__m3_per_sec[:, 0],
+                                                         water_consumption__m3_per_sec[:, 1])
+
+        # dp-table characteristics (steps and boundaries)
         current_mean_temperature__degC = np.mean(start_temperatures__degC)
 
-        boilers_mass__kg = self.number_of_houses * self.model[0].boiler_capacity__m3 * WATER_DENSITY__kg_per_m3
-
-        inst_power_efficient__W = self.max_instantaneous_power__W * self.model[0].efficiency
-        heating_energy_one_step__J = inst_power_efficient__W * step_size__sec
-        delta_temp_mean__degC = (heating_energy_one_step__J /
+        delta_temp_mean__degC = (one_step_heater_energy__J * max_control_steps_inside_15min /
                                  SPECIFIC_HEAT_CAPACITY__J_per_kg_degC /
-                                 boilers_mass__kg)
-        max_possible_consumtion__m3_per_sec = np.max(water_consumption__m3_per_sec[:, 1])
+                                 self.total_water_mass__kg)
+
+        max_possible_consumption__m3_per_sec = np.max(water_consumption__m3_per_sec[:, 1]) * self.number_of_houses
+
         minimum_temperature__degC = (
             current_mean_temperature__degC -
-            WATER_DENSITY__kg_per_m3 * (max_possible_consumtion__m3_per_sec / boilers_mass__kg) *
-            (current_mean_temperature__degC - INCOMING_WATER_TEMPERATURE__degC) * self.horizon_time__sec
+            WATER_DENSITY__kg_per_m3 * (max_possible_consumption__m3_per_sec / self.total_water_mass__kg) *
+            (current_mean_temperature__degC - INCOMING_WATER_TEMPERATURE__degC) * (5 * 3600)
         )
 
         number_of_temp_steps = ceil((self.setpoint__degC - minimum_temperature__degC) / delta_temp_mean__degC)
+        temperature_step__degC = (self.setpoint__degC - minimum_temperature__degC) / (number_of_temp_steps - 1)
 
-        timestamps__sec = np.linspace(0, self.horizon_time__sec, self.horizon__steps) + current_time__sec
-        horizon_price = np.interp(timestamps__sec, energy_price__usd_per_J[:, 0], energy_price__usd_per_J[:, 1])
-        horizon_consumption__m3_per_sec = np.interp(timestamps__sec,
-                                                    water_consumption__m3_per_sec[:, 0],
-                                                    water_consumption__m3_per_sec[:, 1])
-        horizon_consumption__m3_per_sec[:60] *= 0.
+        # functions to set correspondings between temperature and row inside dp-table
+        idx_to_temp = lambda idx: minimum_temperature__degC + idx * temperature_step__degC
+        temp_to_idx = lambda t: int(round((t - minimum_temperature__degC) / temperature_step__degC))
 
-        idx_to_temp = lambda idx: minimum_temperature__degC + idx * delta_temp_mean__degC
-        temp_to_idx = lambda t: int(round((t - minimum_temperature__degC) / delta_temp_mean__degC))
-
-        dp_value = 1e9 * np.ones((number_of_temp_steps + 1, self.horizon__steps + 1))
+        # intialize dp-table and auxilary tables
+        dp_value = 1e9 * np.ones((number_of_temp_steps, self.horizon_periods + 1))
         dp_value[temp_to_idx(current_mean_temperature__degC), 0] = 0
 
+        came_from = -1 * np.ones((number_of_temp_steps, self.horizon_periods + 1), dtype=int)
+        boilers_switched_on = np.zeros((number_of_temp_steps, self.horizon_periods + 1), dtype=int)
 
-        came_from = -1 * np.ones((number_of_temp_steps + 1, self.horizon__steps + 1), dtype=int)
-        boilers_switched_on = np.zeros((number_of_temp_steps + 1, self.horizon__steps + 1), dtype=int)
-        last_15min_energy__J = \
-            np.zeros((number_of_temp_steps + 1, self.horizon__steps + 1, int(15 * 60 / step_size__sec)))
+        # coefficeints to calc functional J
+        j_coeff_delta_t = 3.
+        j_coeff_cons = 1.2
+        j_coeff_price = 1.
 
-        j_coeff_deltaT = 5000.
-
-        for col in range(self.horizon__steps):
-            price = horizon_price[col]
-            consumption__m3 = horizon_consumption__m3_per_sec[col] * step_size__sec
-            consumption__kg = consumption__m3 * WATER_DENSITY__kg_per_m3
+        # filling the dp-value
+        for col in range(self.horizon_periods):
+            price = horizon_price__usd_per_J[col]
+            consumption__kg = horizon_consumption__m3[col] * WATER_DENSITY__kg_per_m3 * self.number_of_houses
 
             for row in range(number_of_temp_steps):
-                energy_already_consumed_in_last_15min__J = np.sum(last_15min_energy__J[row, col, 1:])
                 this_temperature__degC = idx_to_temp(row)
-                for heating__J in allowable_energy_adding__J:
+                for control_steps in allowable_heating_steps:
+                    heating__J = control_steps * one_step_heater_energy__J
 
-                    if energy_already_consumed_in_last_15min__J + heating__J > max_allowable_energy_15min__J:
-                        continue
-
+                    # calculate finctional J value
                     j_new = (
-                        dp_value[row, col] + price * heating__J +
-                        j_coeff_deltaT * consumption__m3 * (self.setpoint__degC - this_temperature__degC)
+                        dp_value[row, col] +
+                        j_coeff_price * price * heating__J +
+                        j_coeff_delta_t * (self.setpoint__degC - this_temperature__degC) +
+                        j_coeff_cons * consumption__kg * (self.setpoint__degC - this_temperature__degC)
                     )
 
+                    # new temperature and new row inside table
                     mean_temperature_new = (
                         this_temperature__degC +
-                        heating__J / SPECIFIC_HEAT_CAPACITY__J_per_kg_degC / boilers_mass__kg +
-                        consumption__kg / boilers_mass__kg * (INCOMING_WATER_TEMPERATURE__degC - this_temperature__degC)
+                        heating__J / SPECIFIC_HEAT_CAPACITY__J_per_kg_degC / self.total_water_mass__kg +
+                        consumption__kg / self.total_water_mass__kg *
+                        (INCOMING_WATER_TEMPERATURE__degC - this_temperature__degC)
                     )
-
                     mean_temperature_new = min(self.setpoint__degC, mean_temperature_new)
                     new_idx = temp_to_idx(mean_temperature_new)
 
+                    # compare and decide if we need to renew dp-value
                     if j_new < dp_value[new_idx, col + 1]:
                         dp_value[new_idx, col + 1] = j_new
                         came_from[new_idx, col + 1] = row
-                        boilers_switched_on[new_idx, col + 1] = int(heating__J / one_heater_power__W / step_size__sec)
-                        last_15min_energy__J[new_idx, col + 1, :-1] = last_15min_energy__J[new_idx, col + 1, 1:]
-                        last_15min_energy__J[new_idx, col + 1, -1] = heating__J
+                        boilers_switched_on[new_idx, col + 1] = control_steps
 
+        # making the heat schedule for 15min periods
+        heating_mean_schedule = np.zeros((self.horizon_periods + 1,), dtype=int)
 
-        heating_mean_schedule = np.zeros((self.horizon__steps + 1,), dtype=int)
-
-        path = np.zeros((self.horizon__steps + 1,), dtype=int)
         idx = np.argmin(dp_value[:, -1])
         for col in range(dp_value.shape[1]-1, -1, -1):
             heating_mean_schedule[col] = boilers_switched_on[idx, col]
-            path[col] = idx
             idx = came_from[idx, col]
 
-        schedule = self.calc_heat_schedule(heating_mean_schedule, start_temperatures__degC, step_size__sec)
+        # refining heat schedule from 15min period to step_size scale
+        schedule = self.calc_heat_schedule(heating_mean_schedule[1:], start_temperatures__degC, step_size__sec)
 
-        print("dp control generated")
         return schedule
-
 
     def calc_heat_schedule(self,
                            mean_schedule,
-                           current_temperatures__degC: NDArray,
+                           temperatures__degC: NDArray,
                            step_size__sec: int,
                            ):
         """
-            Function calculate the heat schedule for microgrid. It receives current temperatures and every timestep
-            heats the boiler with the lowest temperature.
-
-            :param current_temperatures__degC:
-            :param step_size__sec:
-            :return:
+            Function calculate refined heat schedule from general 15min periods' schedule.
+            It receives mean_schedule - the number of boilers switched on during every 15min period
+            and calculates current temperatures for every timestep.
         """
 
-        # # we can simulteneously turn on n_simulteneous boilers
-        # n_simulteneous = int(self.max_instantaneous_power__W / self.heater_power__W)
-        #
-        # # inside 15min period we can turn on boilers for this part of period
-        # heating_time_part = self.max_15min_power__W / (n_simulteneous * self.heater_power__W)
-        # if heating_time_part > 1.:      # no constraints
-        #     heating_time_part = 1.
-        #
-        # # time when n_simulteneous boilers are turned on
-        # heating_inside_15min__sec = int(15 * 60 * heating_time_part)
-        #
-        # # convert time from sec to steps
-        # heating_inside_15min__steps = int(heating_inside_15min__sec / step_size__sec)
-        # nonheating_inside_15min__steps = int(15 * 60 / step_size__sec) - heating_inside_15min__steps
-        #
-        # # part of heating time corresponding to all time (inside 15 min interval)
-        # heat_to_all_relation = (
-        #         heating_inside_15min__steps /
-        #         (heating_inside_15min__steps + nonheating_inside_15min__steps)
-        # )
-        #
-        # # calc deltaT = current_temperature - setpoint for every boiler
-        # temperature_differences__degC = self.setpoint__degC - current_temperatures__degC
-        #
-        # # convert deltaT to time needed to warm up (in sec and steps)
-        # time_to_setpoint__sec = self.temp_to_time__sec_per_degC * temperature_differences__degC
-        # steps_to_setpoint_by_house = np.ceil(time_to_setpoint__sec / step_size__sec).astype(int)
+        # we can simulteneously turn on n_simulteneous boilers
+        n_simulteneous = int(self.max_instantaneous_power__W / self.heater_power__W)
+        steps_inside_period = int(15 * 60 / step_size__sec)
 
-        # calculate schedule to go to setpoint and beyond
-        schedule = np.zeros((self.horizon__steps, self.number_of_houses), dtype=bool)
-
-        # ids_and_steps = [[idx, steps_to_setpoint_by_house[idx]] for idx in range(self.number_of_houses)]
-        # ids_and_steps.sort(key=lambda pair: -pair[1])
-        # steps_to_setpoint = self.horizon__steps
-
-
-        adding_temperature_in_one_step__degC = (
+        # the difference of temperatures added by one switched on
+        add_temperature_in_one_step__degC = (
                 self.model[0].heater_power__W * self.model[0].efficiency * step_size__sec /
                 SPECIFIC_HEAT_CAPACITY__J_per_kg_degC /
                 (self.model[0].water_mass__kg)
         )
 
-        ids_and_temps = [(idx, current_temperatures__degC[idx]) for idx in range(self.number_of_houses)]
-        temperatures__degC = current_temperatures__degC
-        for timestep in range(mean_schedule.shape[0]):
-            for j in range(mean_schedule[timestep]):
+        schedule = np.zeros((0, self.number_of_houses), dtype=bool)
+        for period in range(self.horizon_periods):
+            steps_to_heat = mean_schedule[period]
 
-                min_temp_idx = np.argmin(temperatures__degC)
-                min_temp = temperatures__degC[min_temp_idx]
+            period_schedule = np.zeros((steps_inside_period, self.number_of_houses), dtype=bool)
+            timestep_inside_period = 0
+            while steps_to_heat > 0:
+                for houses_switched_on in range(n_simulteneous):
+                    min_temp_idx = np.argmin(temperatures__degC)
+                    temperatures__degC[min_temp_idx] += add_temperature_in_one_step__degC
+                    period_schedule[timestep_inside_period, min_temp_idx] = True
+                    steps_to_heat -= 1
 
-                if min_temp < self.setpoint__degC:
-                    temperatures__degC[min_temp_idx] += adding_temperature_in_one_step__degC
-                    schedule[timestep, min_temp_idx] = True
+                timestep_inside_period += 1
 
+            schedule = np.vstack((schedule, period_schedule))
 
         return schedule
-
-
-
-
-
-
-        # step = 0
-        # while step < self.horizon__steps:
-        #     for j in range(n_simulteneous):
-        #         idx, steps_needed = ids_and_steps[j]
-        #         heat_steps = min(steps_needed, heating_inside_15min__steps)
-        #         schedule[step: step + heat_steps, idx] = True
-        #         ids_and_steps[j][1] -= heat_steps
-        #
-        #     step += heating_inside_15min__steps
-        #     ids_and_steps.sort(key=lambda pair: -pair[1])
-        #     if ids_and_steps[0][1] < 0:
-        #         # all temperatures are at setpoint
-        #         steps_to_setpoint = min(step, steps_to_setpoint)
-        #     step += nonheating_inside_15min__steps
-        #
-        # return schedule, steps_to_setpoint
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    dpc = DynamicProgramingController()
-    dpc.generate_control(start_temperatures__degC=40*np.ones((5,)),
-                         current_time__sec=3*3600,
-                         step_size__sec=60)
